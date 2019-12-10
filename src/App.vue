a4944c703fa3fc6bd250839fc8019e5828515ecf
soompy <sooper1137@gmail.com>
2019-12-10 18:06:19 +0900

    뷰로 만든 페이지 마크업

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>AZShop</h1>
+      <div class="link">
+         <router-link to="/">Home</router-link> |
+         <router-link to="/about">About</router-link>
+      </div>
+      <a class="basket" href="#"><img src="./assets/basket.png" width="30px" alt="장바구니"> (0)</a>
     </div>
+    
     <router-view/>
+
+    <footer id="footer">
+      <ul class="row">
+        <li class="col-sm-3"><a href="#">홈</a></li>
+        <li class="col-sm-3"><a href="#">카테고리</a></li>
+        <li class="col-sm-3"><a href="#">검색</a></li>
+        <li class="col-sm-3"><a href="#" class="last">마이페이지</a></li>
+      </ul>
+    </footer>
   </div>
 </template>
 
@@ -18,16 +32,72 @@
 }
 
 #nav {
-  background: #f58026;
-  padding: 30px;
+  display: flex;
+  padding: 20px;
+  border-bottom: 1px solid #e0e0e0;
+}
+
+#nav h1 {
+  color: #f4793e;
+  font-weight: bold;
+  font-size: 25px;
+  margin-right: auto;
+}
+
+#nav .link {
+  margin: auto;
 }
 
 #nav a {
+  font-weight: blod;
+  color: #333;
+  font-size: 18px;
+  padding: 0 12px;
+}
+
+#nav .basket {
+  display: block;
+  font-size: 14px;
+  color: #f4793e;
   font-weight: bold;
-  color: #2c3e50;
+  margin-left: auto;
+  vertical-align: middle;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #f4793e;
+}
+
+#footer {
+  border-top: 1px solid #e0e0e0;
+}
+
+#footer a {
+  display: block;
+  color: #333;
+  padding: 20px 0;
+}
+
+#footer a:after {
+  position: absolute;
+  right: 0;  
+  width: 1px;
+  height: 10px;
+  background: #eee;
+  content: "|";
+}
+
+#footer .last:after{
+  display: none;
+}
+
+#footer a:hover {
+  color: #f4793e;
+}
+
+@media (max-width: 575px) {
+  #footer a:after {
+    display: none;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,148 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="inner">
+      <HelloWorld msg="오늘의 추천 상품"/>
+      <ul class="list row">
+        <li class="col-sm-6 item">
+          <div id="product" class="list_view">
+            <figure class="thumb">
+              <div class="product">
+                <img src="../assets/cup.png" alt="cup" width="250px">       
+              </div>
+            </figure>
+            <div class="info">
+              <div class="name"> YETI Rambler 14 oz Stainless Steel Vacuum Insulated Mug with Lid</div>
+              <div class="price">$25</div>
+              <div class="type">
+                <p class="color">color</p>
+                <span>R</span>
+                <span>B</span>
+                <span>Y</span>
+              </div>
+              <button class="cart_btn">Add to Cart</button>
+            </div>
+          </div>
+        </li>
+        <li class="col-sm-6 item">
+          <div id="product" class="list_view">
+            <figure class="thumb">
+              <div class="product">
+                <img src="../assets/watch.png" alt="watch" width="250px">       
+              </div>
+            </figure>
+            <div class="info">
+              <div class="name"> VATI Sport Band Compatible for Apple Watch Band 38mm 40mm 42mm 44mm</div>
+              <div class="price">$5.00 - $35.99</div>
+              <div class="type">
+                <p class="color">color</p>
+                <span>R</span>
+                <span>B</span>
+                <span>Y</span>
+              </div>
+              <button class="cart_btn">Add to Cart</button>
+            </div>
+          </div>
+        </li>
+        <li class="col-sm-6 item">
+          <div id="product" class="list_view">
+            <figure class="thumb">
+              <div class="product">
+                <img src="../assets/controller.png" alt="controller" width="250px">       
+              </div>
+            </figure>
+            <div class="info">
+              <div class="name"> DualShock 4 Wireless Controller for PlayStation 4</div>
+              <div class="price">$46</div>
+              <div class="type">
+                <p class="color">color</p>
+                <span>R</span>
+                <span>B</span>
+                <span>Y</span>
+              </div>
+              <button class="cart_btn">Add to Cart</button>
+            </div>
+          </div>
+        </li>
+        <li class="col-sm-6 item">
+          <div id="product" class="list_view">
+            <figure class="thumb">
+              <div class="product">
+                <img src="../assets/illy.png" alt="illy" width="250px">       
+              </div>
+            </figure>
+            <div class="info">
+              <div class="name"> Illy 60296 y3.2 Espresso and Coffee Machine</div>
+              <div class="price">$99</div>
+              <div class="type">
+                <p class="color">color</p>
+                <span>R</span>
+                <span>B</span>
+                <span>Y</span>
+              </div>
+              <button class="cart_btn">Add to Cart</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+
+<style>
+  .row {
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  .inner {
+    max-width: 1200px;
+    margin: auto; 
+  }
+
+  .list .list_view {
+    display: block;
+    color: #333;
+    border: 1px solid #ccc;
+  }
+
+  .item {
+    margin-bottom: 30px;
+  }
+
+  .info {
+    padding: 10px 10px 20px;
+  }
+
+  .price {
+    margin: 15px 0;
+  }
+
+  .type {
+    display: flex;    
+  }
+
+  .type .color {
+    font-weight: bold;
+    margin-right: 15px;
+  }
+
+  .type span {
+    width: 30px;
+    height: 30px;
+    border: 1px solid #eeeeee;
+    margin: 0 6px 0 0;
+    border-radius: 50%;
+  }
+
+  .cart_btn {
+    margin-top: 20px;
+  }
+</style>
 
 <script>
 // @ is an alias to /src
@@ -15,4 +154,6 @@ export default {
     HelloWorld
   }
 }
+
+
 </script>
